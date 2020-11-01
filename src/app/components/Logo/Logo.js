4350a3528900b4cdc418bc0c39e3ef70d4e30385
Mile Mijatović <mile@milemijatovic.com>
2020-11-01 18:13:16 +0100

    Logo and favicons

---
+++ src/app/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import burgerLogo from '../../../assets/svg/logo.svg';
+
+const Logo = () => {
+  return <img src={burgerLogo} style={{ maxWidth: 50 }} alt="Logo" />;
+};
+
+export default Logo;
--- src/app/components/Navigation/Toolbar/Toolbar.js
+++ src/app/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 
+import Logo from '../../Logo/Logo';
+
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
-      <div>
-        <h1 style={{ color: 'white', fontSize: 25 }}>Burger Builder</h1>
-      </div>
+      <Logo />
       <nav>
         <NavigationItems isAuthenticated={props.isAuth} />
       </nav>
